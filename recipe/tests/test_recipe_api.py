b3c8530be0f07663246646489a23f4361a33af0a
hiwa2719 <hiahmadyan@gmail.com>
2021-04-19 00:02:50 +0430

    added update and partial update tests

--- recipe/tests/test_recipe_api.py
+++ recipe/tests/test_recipe_api.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from rest_framework import status
@@ -72,3 +73,86 @@ class PrivateRecipeApiTest(APITestCase):
         self.assertEqual(response.status_code, 200)
         serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(response.data, serializer.data)
+
+    def test_create_basic_recipe(self):
+        """test creating a recipe"""
+        data = {
+            'title': 'chocolate cheesecake',
+            'time_minutes': 30,
+            'price': 5.00
+        }
+        response = self.client.post(RECIPE_URL, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=response.data['id'])
+        for key in data.keys():
+            self.assertEqual(data[key], getattr(recipe, key))
+        self.assertEqual(recipe.creator, self.user)
+
+    def test_creating_recipe_with_tags(self):
+        """using tags in recipe creation"""
+        tag1 = sample_tag(self.user)
+        tag2 = sample_tag(self.user2, name='tagssss')
+        data = {
+            'title': 'this is a test recipe',
+            'time_minutes': 10,
+            'price': 10.00,
+            'tags': [tag1.id, tag2.id]
+        }
+        response = self.client.post(RECIPE_URL, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=response.data['id'])
+        self.assertIn(tag1, recipe.tags.all())
+        self.assertIn(tag2, recipe.tags.all())
+
+    def test_creating_recipe_with_tags_and_ingredients(self):
+        """adding tags and ingredients to recipe"""
+        tag1 = sample_tag(self.user)
+        tag2 = sample_tag(self.user2, name='tagsss')
+        ingredient1 = sample_ingredient(self.user2)
+        data = {
+            'title': 'Coding is fun',
+            'time_minutes': 10,
+            'price': 50.00,
+            'tags': [tag1.id, tag2.id],
+            'ingredients': [ingredient1.id]
+        }
+        response = self.client.post(RECIPE_URL, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=response.data['id'])
+        self.assertIn(ingredient1, recipe.ingredients.all())
+
+    def test_partial_update_recipe(self):
+        """test recipe partial update"""
+        recipe = sample_recipe(self.user)
+        recipe.tags.add(sample_tag(self.user, name='curry'))
+        new_tag = sample_tag(self.user2, 'hurry')
+        data = {
+            'title': 'updating recipe',
+            'price': 10.00,
+            'tags': [new_tag.id]
+        }
+        response = self.client.patch(recipe_detail_url(recipe.id), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        recipe.refresh_from_db()
+        self.assertEqual(data['title'], recipe.title)
+        tags = recipe.tags.all()
+        self.assertEqual(tags.count(), 1)
+        self.assertIn(new_tag, tags)
+
+    def test_update_recipe(self):
+        """test update recipe"""
+        recipe = sample_recipe(self.user)
+        recipe.tags.add(sample_tag(self.user, name='curry'))
+        data = {
+            'title': 'updating 101',
+            'time_minutes': 10,
+            'price': 10.00,
+        }
+        response = self.client.put(recipe_detail_url(recipe.id), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        recipe.refresh_from_db()
+        for key in data.keys():
+            self.assertEqual(data[key], getattr(recipe, key))
+        tags = recipe.tags.all()
+        self.assertEqual(tags.count(), 0)
+
